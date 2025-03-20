50dfe57b07808d0cd09fbc29f05ba99d7db54ec4
tiesen243 <ttien56906@gmail.com>
2025-03-20 13:11:56 +0700

    chore: minor changes

--- registry/blocks/login-form/components/login-form.tsx
+++ registry/blocks/login-form/components/login-form.tsx
@@ -33,7 +33,7 @@ const loginSchema = z.object({
     ),
 })
 
-const LForm = () => {
+export const LoginForm = () => {
   const form = useForm({
     schema: loginSchema,
     defaultValues: { email: '', password: '' },
@@ -58,69 +58,65 @@ const LForm = () => {
   })
 
   return (
-    <Form form={form}>
-      <FormField
-        name="email"
-        render={(props) => (
-          <FormItem>
-            <FormLabel>Email</FormLabel>
-            <FormControl {...props}>
-              <Input type="email" placeholder="yuki@gmail.com" />
-            </FormControl>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
+    <Card className="w-svh max-w-md overflow-hidden">
+      <CardHeader>
+        <CardTitle>Log in</CardTitle>
+        <CardDescription>
+          Enter your email below to login to your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form form={form}>
+          <FormField
+            name="email"
+            render={(props) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl {...props}>
+                  <Input type="email" placeholder="yuki@gmail.com" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-      <FormField
-        name="password"
-        render={(props) => (
-          <FormItem>
-            <div className="flex items-center justify-between">
-              <FormLabel>Password</FormLabel>
+          <FormField
+            name="password"
+            render={(props) => (
+              <FormItem>
+                <div className="flex items-center justify-between">
+                  <FormLabel>Password</FormLabel>
 
-              <a className="text-sm underline-offset-4 hover:underline">
-                Forgot your password?
-              </a>
-            </div>
-            <FormControl {...props}>
-              <Input type="password" />
-            </FormControl>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
+                  <a className="text-sm underline-offset-4 hover:underline">
+                    Forgot your password?
+                  </a>
+                </div>
+                <FormControl {...props}>
+                  <Input type="password" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-      <Button disabled={form.isPending} type="submit">
-        Login
-      </Button>
-    </Form>
+          <Button disabled={form.isPending} type="submit">
+            Login
+          </Button>
+        </Form>
+      </CardContent>
+
+      <CardFooter className="flex-col">
+        <Button variant="outline" className="w-full">
+          Login with Google
+        </Button>
+
+        <p>
+          Don&apos;t have an account?{' '}
+          <Button variant="link" className="p-0">
+            Sign Up
+          </Button>
+        </p>
+      </CardFooter>
+    </Card>
   )
 }
-
-export const LoginForm: React.FC = () => (
-  <Card className="w-svh max-w-md overflow-hidden">
-    <CardHeader>
-      <CardTitle>Log in</CardTitle>
-      <CardDescription>
-        Enter your email below to login to your account
-      </CardDescription>
-    </CardHeader>
-    <CardContent>
-      <LForm />
-    </CardContent>
-
-    <CardFooter className="flex-col">
-      <Button variant="outline" className="w-full">
-        Login with Google
-      </Button>
-
-      <p>
-        Don&apos;t have an account?{' '}
-        <Button variant="link" className="p-0">
-          Sign Up
-        </Button>
-      </p>
-    </CardFooter>
-  </Card>
-)
