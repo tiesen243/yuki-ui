a2a169c13237956cf3463362e0c1a53f156d305c
tiesen243 <ttien56906@gmail.com>
2025-05-23 03:21:54 +0700

    feat: optimize form component

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,3 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-import '@/env'
-
 import type { NextConfig } from 'next'
 import { createMDX } from 'fumadocs-mdx/next'
 
