0f73894798c62fdaacd34a3017279970b170a6dc
tiesen243 <ttien56906@gmail.com>
2025-05-30 08:52:53 +0700

    feat: update typography

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,28 +1,16 @@
-import { HomeIcon } from 'lucide-react'
-
-import { Badge } from '@/registry/ui/badge'
+import { Typography } from '@/registry/ui/typography'
 import { ToggleTheme } from './page.client'
 
 export default function TestPage() {
   return (
     <main className="container flex min-h-dvh flex-col items-center justify-center gap-4">
       <ToggleTheme />
-
       {(
-        [
-          'default',
-          'secondary',
-          'success',
-          'info',
-          'warning',
-          'destructive',
-          'outline',
-        ] as const
+        ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'blockquote', 'code'] as const
       ).map((variant) => (
-        <Badge key={variant} variant={variant} size="lg" rounded="full">
-          <HomeIcon />
-          {variant}
-        </Badge>
+        <Typography key={variant} variant={variant} className="w-full">
+          The lazy dog jumped over the quick brown fox.
+        </Typography>
       ))}
     </main>
   )
