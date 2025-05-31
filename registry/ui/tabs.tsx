4a4618288cce05d949aba36294388ea99bbef204
tiesen243 <ttien56906@gmail.com>
2025-05-31 07:42:21 +0700

    feat: update tabs components

--- registry/ui/tabs.tsx
+++ registry/ui/tabs.tsx
@@ -26,13 +26,11 @@ const tabsListVariants = cva(
     variants: {
       variant: {
         default:
-          'bg-muted text-muted-foreground *:data-[slot=tabs-trigger]:data-[state=active]:bg-background *:data-[slot=tabs-trigger]:dark:data-[state=active]:bg-input/30 *:data-[slot=tabs-trigger]:dark:data-[state=active]:border-input *:data-[slot=tabs-trigger]:rounded-md *:data-[slot=tabs-trigger]:border *:data-[slot=tabs-trigger]:data-[state=active]:shadow-sm',
+          'bg-muted *:data-[slot=tabs-trigger]:data-[state=active]:bg-background *:data-[slot=tabs-trigger]:dark:data-[state=active]:bg-input/30 *:data-[slot=tabs-trigger]:dark:data-[state=active]:border-input *:data-[slot=tabs-trigger]:rounded-md *:data-[slot=tabs-trigger]:border *:data-[slot=tabs-trigger]:data-[state=active]:shadow-sm',
         underline:
-          'text-muted-foreground *:data-[slot=tabs-trigger]:data-[state=active]:border-primary bg-transparent *:data-[slot=tabs-trigger]:data-[state=active]:border-b',
+          '*:data-[slot=tabs-trigger]:data-[state=active]:border-primary *:data-[slot=tabs-trigger]:border-b',
         border:
-          '*:data-[slot=tabs-trigger]:dark:data-[state=active]:bg-muted *:data-[slot=tabs-trigger]:border-border border *:data-[slot=tabs-trigger]:rounded-md *:data-[slot=tabs-trigger]:data-[state=active]:border *:data-[slot=tabs-trigger]:dark:border-transparent',
-        light:
-          '*:data-[slot=tabs-trigger]:data-[state=active]:bg-muted *:data-[slot=tabs-trigger]:rounded-md *:data-[slot=tabs-trigger]:data-[state=active]:shadow-sm',
+          '*:data-[slot=tabs-trigger]:dark:data-[state=active]:bg-muted *:data-[slot=tabs-trigger]:border-border *:data-[slot=tabs-trigger]:data-[state=active]:bg-background border *:data-[slot=tabs-trigger]:rounded-md *:data-[slot=tabs-trigger]:data-[state=active]:border',
       },
     },
     defaultVariants: {
@@ -63,7 +61,7 @@ function TabsTrigger({
     <TabsPrimitive.Trigger
       data-slot="tabs-trigger"
       className={cn(
-        "focus-visible:border-ring focus-visible:ring-ring/50 dark:data-[state=active]:text-foreground focus-visible:outline-ring text-foreground dark:text-muted-foreground dark:data-[state=inactive]:hover:text-muted-foreground/80 data-[state=inactive]:hover:text-muted-foreground inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 border-transparent px-2 py-1 text-sm font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 data-[state=inactive]:cursor-pointer [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+        "focus-visible:border-ring focus-visible:ring-ring/50 dark:data-[state=active]:text-foreground focus-visible:outline-ring text-foreground dark:text-muted-foreground dark:data-[state=inactive]:hover:text-muted-foreground/60 data-[state=inactive]:hover:text-foreground/60 inline-flex h-[calc(100%-1px)] flex-1 cursor-pointer items-center justify-center gap-1.5 border-transparent px-2 py-1 text-sm font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
         className,
       )}
       {...props}
