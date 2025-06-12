92ea2d39119abe1d939143264522155986695a62
tiesen243 <ttien56906@gmail.com>
2025-06-12 11:19:46 +0700

    chore: minor changes

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,7 +1,7 @@
 import type { VariantProps } from 'class-variance-authority'
 import * as React from 'react'
-import { Slot as SlotPrimitive } from 'radix-ui';
 import { cva } from 'class-variance-authority'
+import { Slot as SlotPrimitive } from 'radix-ui'
 
 import { cn } from '@/lib/utils'
 
