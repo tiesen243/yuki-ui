b753378b34aa1ff054602b2f766c5d92b88f9836
tiesen243 <ttien56906@gmail.com>
2025-06-22 21:55:59 +0700

    feat: rebuild

---
+++ kaze/app/layout.tsx
@@ -0,0 +1,39 @@
+import '@/app/globals.css'
+
+import { Geist, Geist_Mono } from 'next/font/google'
+
+import { cn, ThemeProvider } from '@yuki/ui'
+
+import { createMetadata } from '@/lib/metadata'
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          'flex min-h-dvh flex-col font-sans antialiased',
+          geistSans.variable,
+          geistMono.variable,
+        )}
+      >
+        <ThemeProvider attribute="class" disableTransitionOnChange enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
+
+export const metadata = createMetadata({ title: 'Next.js' })
