81d1b473df64e89581759628769921cbc2b1175c
tiesen243 <ttien56906@gmail.com>
2025-06-23 20:30:44 +0700

    chore: minor changes

--- kaze/app/layout.config.tsx
+++ kaze/app/layout.config.tsx
@@ -1,6 +1,8 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared'
 import Image from 'next/image'
 
+import { GalleryThumbnailsIcon } from '@yuki/ui/icons'
+
 import Tiesen from '@/public/assets/tiesen.png'
 
 export const baseOptions: BaseLayoutProps = {
@@ -11,8 +13,17 @@ export const baseOptions: BaseLayoutProps = {
         alt="Tiesen Logo"
         className="w-2/3 object-cover"
         sizes="(max-width: 768px) 45vw, (max-width: 1200px) 33vw, 20vw"
+        priority
       />
     ),
   },
   githubUrl: 'https://github.com/tiesen243/yuki-ui',
+  links: [
+    {
+      icon: <GalleryThumbnailsIcon />,
+      type: 'icon',
+      text: 'Portfolio',
+      url: 'https://tiesen.id.vn',
+    },
+  ],
 }
