544d73ea53f333d0e2d8f1f1c113fad1adeb124f
tiesen243 <ttien56906@gmail.com>
2025-06-23 19:59:51 +0700

    fix: logo size

--- kaze/app/layout.config.tsx
+++ kaze/app/layout.config.tsx
@@ -6,7 +6,12 @@ import Tiesen from '@/public/assets/tiesen.png'
 export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
-      <Image src={Tiesen} alt="Tiesen Logo" className="w-2/3 object-cover" />
+      <Image
+        src={Tiesen}
+        alt="Tiesen Logo"
+        className="w-2/3 object-cover"
+        sizes="(max-width: 768px) 45vw, (max-width: 1200px) 33vw, 20vw"
+      />
     ),
   },
   githubUrl: 'https://github.com/tiesen243/yuki-ui',
