f4d8a817b46f588d5a2a75204fdfc149598bb15c
tiesen243 <ttien56906@gmail.com>
2025-11-28 10:16:42 +0700

    feat: added tsdown

---
+++ packages/ui/tsdown.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'tsdown'
+
+export default defineConfig({
+  entry: ['./src/index.ts', './src/components/*.tsx', './src/hooks/*.tsx'],
+  dts: true,
+  shims: true,
+  exports: {
+    customExports() {
+      return {
+        '.': './dist/index.mjs',
+        './*': {
+          default: './dist/components/*.mjs',
+          types: './dist/components/*.d.mts',
+        },
+        './hooks/*': {
+          default: './dist/hooks/*.mjs',
+          types: './dist/hooks/*.d.mts',
+        },
+        './tailwind.css': './src/tailwind.css',
+        './package.json': './package.json',
+      }
+    },
+  },
+})
