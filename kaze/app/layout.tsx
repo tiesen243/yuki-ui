0a6e2c08de3dfe139506d63e01ed57340bfe3fc7
tiesen243 <ttien56906@gmail.com>
2025-11-29 15:56:50 +0700

    chore: minor changes

--- kaze/app/layout.tsx
+++ kaze/app/layout.tsx
@@ -1,6 +1,6 @@
 import '@/app/globals.css'
 
-import { Geist, Geist_Mono, Yuji_Syuku } from 'next/font/google'
+import { Geist, Geist_Mono } from 'next/font/google'
 import { RootProvider } from 'fumadocs-ui/provider/next'
 
 import { cn } from '@yuki/ui'
@@ -11,12 +11,6 @@ import { createMetadata } from '@/lib/metadata'
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
-})
-
-const yujiSyuky = Yuji_Syuku({
-  variable: '--font-yuji-syuku',
-  subsets: ['latin'],
-  weight: '400',
 })
 
 const geistMono = Geist_Mono({
@@ -31,7 +25,6 @@ export default function RootLayout({ children }: LayoutProps<'/'>) {
         className={cn(
           'flex min-h-dvh flex-col font-sans antialiased',
           geistSans.variable,
-          yujiSyuky.variable,
           geistMono.variable,
         )}
       >
