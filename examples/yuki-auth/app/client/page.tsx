85ac0d8a5af0e8f75d59d62903e8039707739b07
tiesen243 <ttien56906@gmail.com>
2025-12-15 22:01:06 +0700

    chore: remove radix

--- examples/yuki-auth/app/client/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Profile } from '@/app/client/page.client'
-
-export default function ClientPage() {
-  return (
-    <main className='container py-6'>
-      <Profile />
-    </main>
-  )
-}
