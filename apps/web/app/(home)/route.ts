ff6bf3a8dd70e51ab02125fd6f6c911e6e6da0ca
tiesen243 <ttien56906@gmail.com>
2026-04-14 22:13:27 +0700

    fix: home

--- apps/web/app/(home)/route.ts
+++
@@ -1,7 +0,0 @@
-import type { NextRequest } from 'next/server'
-
-import { NextResponse } from 'next/server'
-
-export function GET(req: NextRequest) {
-  return NextResponse.rewrite(new URL('/docs', req.url))
-}
