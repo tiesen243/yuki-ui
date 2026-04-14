be9400416dbee9bc5e0335cdbf0e440905a4a8e3
tiesen243 <ttien56906@gmail.com>
2026-04-14 13:19:03 +0700

    style: update font

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,17 +1,30 @@
 import '@/styles/globals.css'
 
-import { Inter } from 'next/font/google'
+import { Geist, Geist_Mono } from 'next/font/google'
 
 import { Provider } from '@/components/provider'
+import { cn } from '@/lib/utils'
 
-const inter = Inter({
+const geist = Geist({
   subsets: ['latin'],
+  variable: '--font-geist-sans',
+})
+
+const geistMono = Geist_Mono({
+  subsets: ['latin'],
+  variable: '--font-geist-mono',
 })
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
-    <html lang='en' className={inter.className} suppressHydrationWarning>
-      <body className='flex min-h-screen flex-col'>
+    <html lang='en' suppressHydrationWarning>
+      <body
+        className={cn(
+          geist.variable,
+          geistMono.variable,
+          'flex min-h-screen flex-col font-sans antialiased'
+        )}
+      >
         <Provider>{children}</Provider>
       </body>
     </html>
