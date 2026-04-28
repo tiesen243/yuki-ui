ae61fd0a992116e115291e3d1bf483d2b4ff4897
tiesen243 <ttien56906@gmail.com>
2026-04-29 00:13:22 +0700

    chore: minor changes

--- apps/web/registry/examples/typography.tsx
+++ apps/web/registry/examples/typography.tsx
@@ -9,8 +9,6 @@ export default function TypographyDemo() {
       <Typography variant='h2'>H2. Heading 2</Typography>
       <Typography variant='h3'>H3. Heading 3</Typography>
       <Typography variant='h4'>H4. Heading 4</Typography>
-      <Typography variant='h5'>H5. Heading 5</Typography>
-      <Typography variant='h6'>H6. Heading 6</Typography>
       <Typography>{text}</Typography>
       <Typography variant='blockquote'>{text}</Typography>
       <Typography variant='code'>{text}</Typography>
@@ -24,6 +22,18 @@ export default function TypographyDemo() {
         <li>Ordered item 2</li>
         <li>Ordered item 3</li>
       </Typography>
+
+      <figure>
+        {/* oxlint-disable-next-line next/no-img-element */}
+        <img
+          src='https://images.unsplash.com/photo-1700199849610-dd069b1719c2?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+          alt='demonstration of typography styles'
+          className='mx-auto w-1/2 rounded-md object-cover'
+        />
+        <Typography variant='caption' render={<figcaption />}>
+          Figure 1. A sample image demonstrating typography styles.
+        </Typography>
+      </figure>
     </article>
   )
 }
