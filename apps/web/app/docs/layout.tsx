cbd7db275ce0e36218ec8688e4642576d168d85a
tiesen243 <ttien56906@gmail.com>
2026-05-10 22:44:14 +0700

    chore: minor changes

--- apps/web/app/docs/layout.tsx
+++ apps/web/app/docs/layout.tsx
@@ -1,16 +1,11 @@
 import { DocsLayout } from '@fumadocs/base-ui/layouts/notebook'
 
-import { Header } from '@/layouts/notebook/slots/header'
 import { baseOptions } from '@/lib/layout.shared'
 import { source } from '@/lib/source'
 
 export default function Layout({ children }: LayoutProps<'/docs'>) {
   return (
-    <DocsLayout
-      tree={source.getPageTree()}
-      {...baseOptions()}
-      slots={{ header: Header }}
-    >
+    <DocsLayout tree={source.getPageTree()} {...baseOptions()}>
       {children}
     </DocsLayout>
   )
