1e1467872f9dca140a638ec1bddd8cefb94e2671
tiesen243 <ttien56906@gmail.com>
2026-06-27 20:18:52 +0700

    chore: bump deps

--- apps/web/registry/ui/typography.tsx
+++ apps/web/registry/ui/typography.tsx
@@ -8,15 +8,15 @@ const typographyVariants = cva('text-base font-normal', {
   variants: {
     variant: {
       h1: 'scroll-m-20 text-4xl font-extrabold tracking-tight text-balance',
-      h2: 'my-4 scroll-m-20 text-2xl font-bold tracking-tight text-balance',
-      h3: 'my-2 scroll-m-20 text-xl font-semibold tracking-tight text-balance',
-      h4: 'my-2 scroll-m-20 text-lg font-semibold tracking-tight text-balance',
+      h2: 'scroll-m-20 text-2xl font-bold tracking-tight text-balance',
+      h3: 'scroll-m-20 text-xl font-semibold tracking-tight text-balance',
+      h4: 'scroll-m-20 text-lg font-semibold tracking-tight text-balance',
       p: 'text-justify leading-7 text-pretty',
       small: 'block text-sm leading-none font-medium tracking-wide',
-      ul: 'my-4 ml-6 list-disc text-base [&>li]:mt-2 [&>li]:first:mt-0',
-      ol: 'my-4 ml-6 list-decimal text-base [&>li]:mt-2 [&>li]:first:mt-0',
+      ul: 'ml-6 list-disc text-base [&>li]:mt-2 [&>li]:first:mt-0',
+      ol: 'ml-6 list-decimal text-base [&>li]:mt-2 [&>li]:first:mt-0',
       blockquote:
-        'my-4 inline-block border-l-2 pl-6 italic before:content-["“"] after:content-["”"]',
+        'inline-block border-l-2 pl-6 italic before:content-["“"] after:content-["”"]',
       code: 'relative w-fit rounded-sm border border-accent bg-accent/40 px-[0.3rem] py-[0.2rem] font-mono text-sm font-medium text-accent-foreground',
       caption:
         'mt-1 block text-center text-sm tracking-wide text-muted-foreground',
